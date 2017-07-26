cised-ca/se-dir-frontend-admin-react

commit 60df492a116e06cf5ee26bf240747167d8ab7662

Implement "Are you sure?" enterprise deletion modal

src/components/panels/EnterpriseDetailsPanelComponent.js
@@ -1,6 +1,9 @@
 'use strict';
 
 import React from 'react';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#app');
 
 require('styles/panels/EnterpriseDetailsPanel.scss');
 
@@ -8,16 +11,28 @@ class EnterpriseDetailsPanelComponent extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = props.enterprise;
+    this.state = {
+      enterprise: props.enterprise,
+      modalIsOpen: false
+    };
+
+    this.closeModal = this.closeModal.bind(this);
+    this.deleteEnterprise = this.deleteEnterprise.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmitForm = this.handleSubmitForm.bind(this);
     this.handleDeleteEnterprise = this.handleDeleteEnterprise.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
   }
 
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.enterprise.name !== this.props.enterprise.name) {
-      this.setState(nextProps.enterprise);
+      this.setState({
+        enterprise: nextProps.enterprise
+      });
     }
   }
 
@@ -27,33 +42,40 @@ class EnterpriseDetailsPanelComponent extends React.Component {
     this.props.setActivePanel(2);
   }
 
+  deleteEnterprise() {
+    const enterprise = this.state.enterprise;
+    const endpoint = this.context.config.api_root + '/enterprise/' + enterprise.id;
+
+    const request = new Request(endpoint, {
+      method: 'DELETE'
+    });
+
+    fetch(request)
+      .then((response) => {
+        // TODO: Display success
+        // TODO: Refresh list of enterprises
+
+        // Set active panel to the enterprise list
+        this.props.setActivePanel(2);
+      })
+      .catch((error) => {
+        // TODO: Display error
+    });
+  }
+
   handleDeleteEnterprise(event) {
-      // TODO: "Are you sure" modal
+    event.preventDefault();
 
-      event.preventDefault();
-
-      const enterprise = this.state;
-      const endpoint = this.context.config.api_root + '/enterprise/' + enterprise.id;
-
-      const request = new Request(endpoint, {
-        method: 'DELETE'
-      });
-
-      fetch(request)
-        .then((response) => {
-          // TODO: Display success
-          // TODO: Refresh list of enterprises
-          // TODO: Set active panel to the enterprise list
-        })
-        .catch((error) => {
-          // TODO: Display error
-        });
+    // Are you sure modal
+    this.setState({
+      modalIsOpen: true
+    });
   }
 
   handleSubmitForm(event) {
     event.preventDefault();
 
-    let enterprise = this.state;
+    let enterprise = this.state.enterprise;
     const endpoint = this.context.config.api_root + '/enterprise/' + enterprise.id;
 
     let headers = new Headers();
@@ -86,16 +108,42 @@ class EnterpriseDetailsPanelComponent extends React.Component {
     const target = event.target;
 
     this.setState({
-      [target.name]: target.value
+      enterprise: {
+        [target.name]: target.value
+      }
     });
   }
 
   render() {
-    const enterprise = this.state;
+    const enterprise = this.state.enterprise;
 
     // TODO: Add missing fields
     return (
       <div className="panel panel--wide enterprisedetailspanel-component">
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          contentLabel="Are you sure you want to delete {enterprise.name}?"
+        >
+
+          <h2 className="modal__title" ref={subtitle => this.subtitle = subtitle}>
+            Are you sure you want to delete {enterprise.name}?
+          </h2>
+
+          <p>
+            You are about to delete {enterprise.name}. This cannot be undone.
+            Are you sure you'd like to proceed?
+          </p>
+
+          <input className="button button--destructive" name="delete"
+            onClick={this.deleteEnterprise}
+            type="button" value="Delete" />
+
+          <input className="button button--default" name="cancel"
+            onClick={this.closeModal}
+            type="button" value="Cancel" />
+        </Modal>
+
         <h1>{enterprise.name}</h1>
 
         <form onSubmit={this.handleSubmitForm}>

src/components/Main.js
@@ -1,6 +1,9 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
 
+/* Imported here since we don't have our own Modal component (yet?) */
+require('styles/Modal.scss');
+
 import React from 'react';
 
 import { browserHistory, Router, Route, IndexRoute } from 'react-router';

src/styles/Modal.scss
@@ -1,0 +1,4 @@
+/* Imported in Main.js since we don't have our own Modal component (yet?) */
+.modal__title {
+  margin-top: 0;
+}

src/styles/StatusList.scss
@@ -4,8 +4,8 @@
   padding: 0;
 
   .status-list-item {
-    cursor: pointer;
     border-bottom: 1px solid #000;
+    cursor: pointer;
 
     &:hover {
       background-color: #ddd;

src/styles/panels/EnterpriseDetailsPanel.scss
@@ -4,31 +4,42 @@
   h1 {
     margin-top: 0;
   }
+}
 
-  label {
-    display: block;
-    font-weight: bold;
-    margin-bottom: 20px;
-  }
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 20px;
+}
 
+input,
+textarea {
+  display: block;
+  margin-top: 10px;
+  padding: 8px;
+  width: 100%;
+}
+
+/*
+  normalize.css conflicts with the following (sets "font: inherit") when we
+  remove the wrapping class specificity...
+*/
+.app {
   input,
   textarea {
-    display: block;
     font-weight: normal;
-    margin-top: 10px;
-    padding: 8px;
-    width: 100%;
   }
+}
 
-  textarea {
-    min-height: 100px;
-  }
+textarea {
+  min-height: 100px;
 }
 
 .button {
   background: none;
   border: 0;
   cursor: pointer;
+  margin-top: 10px;
 }
 
 .button--default {
@@ -69,7 +80,7 @@
     &[type="submit"],
     &[type="button"] {
       display: inline-block;
-      margin-right: 10px;
+      margin: 0 10px 0 0;
       width: auto;
     }
   }

commit 6f0c3075369894417b7f6ae0a0c0cbc6d7bf8889

Fix enterprise editing
Broken in 60df492a116 where the enterprise object was inadvertently
overwritten by the last edited value.

src/components/panels/EnterpriseDetailsPanelComponent.js
@@ -108,10 +108,11 @@ class EnterpriseDetailsPanelComponent extends React.Component {
   handleInputChange(event) {
     const target = event.target;
 
+    let curr_enterprise = this.state.enterprise;
+    curr_enterprise[target.name] = target.value;
+
     this.setState({
-      enterprise: {
-        [target.name]: target.value
-      }
+      enterprise: curr_enterprise
     });
   }
 